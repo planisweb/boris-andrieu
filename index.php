aa8f086572f709d2c0c907305107075df14c0219
Robi <95742139+Bozy87@users.noreply.github.com>
2023-08-10 17:35:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="site de boris andrieu">
+    <meta name="author" content="Boris Andrieu">
+
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="./css/robi.css">
+  
+    <style>
+        @import url('https://fonts.cdnfonts.com/css/ds-marker-felt');
+    </style>
+   <link rel="icon"  href="./img/PWB.png" />
+    <!--   FONT  ---->
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <!---  BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
+    <!------          --------->
+      <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="./js/mange.js" async=""></script>
+    <title>Boris ANDRIEU</title>
+</head>
+
+<header>
+
+    <!--  Liens logo  -->
+
+    <div id="hautDePage">
+        <div id="liens" class="ouvreliens">
+            <ul>
+                <li>
+                    <a href="https://drive.google.com/file/d/1Mw2mvALvteLtzEYwnhuUWicCjzTNj66u/view?usp=sharing"
+                        target="_blank"><img id="gm" src="./img/cv.png" alt="liengmail"></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/boris-andrieu-74b28625b/" target="_blank"><img id="link"
+                            src="./img/linked.png" alt="lien linkedin"></a>
+                </li>
+
+                <li>
+                    <a href="https://github.com/Bozy87" target="_blank"><img id="git" src="./img/github.png"
+                            alt="lien github"></a>
+
+                </li>
+
+            </ul>
+        </div>
+
+    </div>
+    <!--  MENUS NAV  -->
+    <div>
+        <nav>
+            <ul id="menus">
+                <li><a id="m1" class=" menui" href="#presentation">Qui </a></li>
+
+                <li><a id="m2" class=" menui" href="#presentation2">Quoi</a></li>
+
+
+
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<!---   CONTENU    -->
+
+<body>
+    <h1 class="invis">boris andrieu limoges; Boris ANDRIEU; boris.andrieu</h1>
+<h2 class="invis">boris andrieu limoges; Boris ANDRIEU; boris.andrieu</h2>
+    <div id="docentier">
+        <div id="presentation" class="parapresentation">
+            <a id="haut"></a>
+
+
+
+            <div class="englobe">
+                <img id="moi" class="d-block mx-auto" src="./img/nameis.png" alt="photodemoi">
+                <h2 class="slt">Apprenti développeur web</h2>
+
+            </div>
+            <div id="citation" data-aos="flip-up">
+                <p class="fontstyle"> “Mon apprentissage n’a d’autre fruit <br>que de me faire sentir combien il me
+                    reste à
+                    apprendre.”</p><br> Montaigne
+            </div>
+            <div id="partie1" class="row" data-aos="fade-up">
+                <div class="col-12 col-sm-6">
+                    <div id="col1">
+                        <a id="tmoipeur"><br>2019, premiers pas dans le web...</a><br>
+                        <img id="moipeur" class="bordrond" src="./img/moipeur.png" alt="peur dans le fire">
+
+
+
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 ">
+                    <div id="col2">
+                        <br><br><br>
+                        <img id="moimieux" class="bordrond" src="./img/moimieux.png" alt="moins peur dans le fire">
+                        <a id="tmoimieux"><br>2023, j'y vois beaucoup plus clair !</a>
+                    </div>
+                </div>
+            </div>
+            <div data-aos="fade-up">
+                <br>
+                <div id="p1">
+                    <p>Après beaucoup de temps passé à <strong>étudier, chercher, reproduire;</strong><br> j'ai fini par acquérir
+                        certains
+                        automatismes.<br>
+                        Je suis aujourd'hui prêt à <strong>proposer mes services </strong>et mettre à profit mes
+                        connaissances,<br> seul ou en équipe.</p><br><br>
+                </div>
+                <div id="carte1" class=" tile">
+                    <div id="avant" class=" tile__face tile__face--front">
+                        <h2>Niveau correct :</h2>
+                        * HTML/CSS<br>
+                        * Wordpress<br>
+                        * Gestion d'hébergement<br>
+                        * Traitement texte/ image<br>
+                        * Référencement/ SEO<br><br>
+                    </div>
+                    <div id="arriere" class=" tile__face tile__face--back">
+                        <h2>En progression :</h2><br><br>
+                        * JAVASCRIPT<br>
+                        * Commandes GIT<br>
+                        * Bases de données<br><br>
+                    </div>
+                </div><br>
+                <div id="carte2" class=" row">
+                    <div id="avant2" class="col-sm-12">
+                        <h2>Niveau correct :</h2>
+                        * HTML/CSS<br>
+                        * Wordpress<br>
+                        * Gestion d'hébergement<br>
+                        * Traitement texte/ image<br>
+                        * Référencement/ SEO<br>
+                    </div><br>
+                    <div id="arriere2" class="col-sm-12">
+                        <h2>En progresion :</h2>
+                        * JAVASCRIPT<br>
+                        * Commandes GIT<br>
+                        * Bases de données<br><br>
+                    </div>
+                </div><br>
+            </div>
+            <div data-aos="fade-up">
+                <p> <strong>Autodidacte</strong>, regisseur technique de formation,<br> mon parcours m'amène peu à peu
+                    dans le
+                    <strong>developpement</strong> et l'<strong>intégration de contenu web</strong> <br>qui regroupent
+                    tout ce que
+                    j'aime:<br>
+                </p>
+
+
+
+                <div class="fontstyle crea" data-aos="fade-right">
+
+                    création, adaptation, technologies,</div> <br>
+                <div class="fontstyle cha" data-aos="fade-right">
+                    challenges, et opportunités.
+                </div>
+
+
+            </div>
+        </div>
+        <!--  PARTIE 2  -->
+
+        <!--  BODY CONTENU  -->
+        <h1 class="invis">boris andrieu limoges; Boris ANDRIEU; boris.andrieu</h1>
+        <h2 class="invis">boris andrieu limoges; Boris ANDRIEU; boris.andrieu</h2>
+        <div id="presentation2">
+            <div data-aos="fade-up">
+                <br>
+                <div id="partie2" class="englobe">
+                    <img class="bordrond d-block mx-auto" src="./img/yamini.jpg" alt="photomontage">
+                    <h2 class="slt">Choses et autres.</h2>
+                    <br>
+
+                </div>
+            </div>
+            <div data-aos="fade-up">
+                <p>Je vous invite à découvrir <strong>quelques-uns <br> de mes travaux : </strong><br>
+                    (Cliquez sur les images)</p>
+                <!--- Fusion interim  --->
+                <div class="row">
+                    <div class="col-12 col-sm-4">
+                        <h4> Site vitrine pour une agence d'intérim</h4>
+                        <br> <a href="https://www.fusion-interim.fr" target="_blank">
+                            <img class="prods" src="./img/fusion.png" alt="image site fusion interim"> </a>
+                        <br>
+                    </div> 
+                    <!--- wordpress -->
+                    <div class="col-12 col-sm-4">
+                        <h4> Petit blog sous Wordpress</h4>
+                        <br>
+                        <a href="https://perenne.borisandrieu.go.yo.fr/" target="_blank">
+                            <img class="prods" src="./img/perenne.png" alt="image site perenne.net"> </a> <br>
+                        <br>
+                    </div>
+                       <!----  RUDI -->
+                       <div class="col-12 col-sm-4">
+                        <h4> Site vitrine Artiste</h4>
+                        <br> <a href="https://lentrepot.github.io/RUDI/" target="_blank">
+                            <img class="prods" src="./img/rudi1.png" alt="image site rudi"> </a>
+
+                        <br>
+                    </div>
+                  
+                   
+                </div>
+
+                <div class="row">
+                      <!--- wordpress rapport -->
+                    <div class="col-12 col-sm-4">
+                        <h4> Rapport de stage</h4>
+                        <br>
+                        <a href="https://latelier.go.yo.fr/WP02/" target="_blank">
+                            <img  class="prods" src="./img/rapport.png" alt="image site rapport de stage"> </a> <br>
+                        <br>
+                    </div>
+                    <!--- ODB WIX -->
+                    <div class="col-12 col-sm-4">
+                        <h4> Ebauche de site avec WIX</h4>
+                        <br>
+                        <a href="https://bozyboite.wixsite.com/outils-dev-web" target="_blank">
+                            <img class="prods" src="./img/wix.png" alt="image site wix"> </a>
+
+                        <br>
+                    </div>
+                 
+                  
+                    <!----  BOOTSTRAP -->
+                    <div class="col-12 col-sm-4">
+                        <h4> Test du framework "Bootstrap"</h4>
+                        <br>
+                        <a href="./Bootstrap/index.html" target="_blank">
+                            <img class="prods" src="./img/whynot.png" alt="image site bootstrap"> </a>
+
+                        <br>
+                    </div>
+                                        
+                </div>
+            </div>
+        </div>
+        <br>
+
+
+    </div>
+
+
+
+
+    <p class="fontstyle" data-aos="zoom-in" data-aos-duration="3000"> Quelque soit votre besoin, votre projet, n'hésitez
+        pas à me contacter, <br>
+        et nous trouverons ensemble les solutions les plus adaptées !</p>
+
+       
+
+    <!--  FOOTER  -->
+    <footer>
+
+        <ul id="basdepage">
+            <li><a href="mailto:borisandrieu87@gmail.com">Contact</a><br><a>borisandrieu87@gmail.com</a></li>
+
+            <li><a href="https://www.legifrance.gouv.fr/loda/id/JORFTEXT000000801164" target="_blank">Mentions
+                    légales</a></li>
+        </ul>
+
+    </footer>
+
+
+</body> 
+
+</html>
+
+<form method="post">
+        <label>Votre email</label>
+        <input type="email" name="email" required><br>
+        <label>Message</label>
+        <textarea name="message" required></textarea><br>
+        <input type="submit">
+    </form>
+
+
+
+
+<?php
+    if (isset($_POST['message'])) {
+        $retour = mail('planisweb@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: audioboite@gmail.com' . "\r\n" . 'Reply-to: ' . $_POST['email']);
+        if($retour)
+            echo '<p>Votre message a bien été envoyé.</p>';
+    }
+    ?>
+    <script>
+        AOS.init();
+      </script>
+</body>
+</html>
